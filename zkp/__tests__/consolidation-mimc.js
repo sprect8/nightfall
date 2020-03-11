4c856574f144ad08566ed8e3dd4d0f760c2a7ab2
Liju Jose <liju.jose@gds.ey.com>
2020-03-11 13:04:50 +0530

    fix(zkp): unit test fix if HASH_TYPE is sha

--- zkp/__tests__/consolidation-mimc.js
+++ zkp/__tests__/consolidation-mimc.js
@@ -72,134 +72,141 @@ let fTokenShieldJson;
 let fTokenShieldAddress;
 let erc20Address;
 
-beforeAll(async () => {
-  if (!(await bc.isConnected())) await bc.connect();
-  accounts = await (await bc.connection()).eth.getAccounts();
-  const { contractJson, contractInstance } = await getTruffleContractInstance('FTokenShield');
-  erc20Address = await getContractAddress('FToken');
-  const erc20AddressPadded = `0x${utils.strip0x(erc20Address).padStart(64, '0')}`;
-  fTokenShieldAddress = contractInstance.address;
-  fTokenShieldJson = contractJson;
-  for (let i = 0; i < PROOF_LENGTH; i++) {
-    pkB[i] = utils.strip0x(utils.hash(skB[i]));
-  }
-  pkB = await Promise.all(pkB);
-  S_A_C = await utils.rndHex(32);
-  pkA = utils.strip0x(utils.hash(skA));
-  Z_A_C = utils.concatenateThenHash(erc20AddressPadded, C, pkA, S_A_C);
-});
-
-// eslint-disable-next-line no-undef
-describe('f-token-controller.js tests', () => {
-  // Alice has C + D to start total = 50 ETH
-  // Alice sends Bob E and gets F back (Bob has 40 ETH, Alice has 10 ETH)
-  // Bob then has E+G at total of 70 ETH
-  // Bob sends H to Alice and keeps I (Bob has 50 ETH and Alice has 10+20=30 ETH)
-  test('Should be correcly configurated to use MiMC', async () => {
-    expect(process.env.HASH_TYPE).toEqual('mimc');
-  });
-  test('Should create 10000 tokens in accounts[0]', async () => {
-    // fund some accounts with FToken
-    const AMOUNT = 10000;
-    const bal1 = await controller.getBalance(accounts[0]);
-    await controller.buyFToken(AMOUNT, accounts[0]);
-    const bal2 = await controller.getBalance(accounts[0]);
-    expect(AMOUNT).toEqual(bal2 - bal1);
-  });
-
-  test('Should mint an ERC-20 commitment Z_A_C for Alice of value C', async () => {
-    const { commitment: zTest, commitmentIndex: zIndex } = await erc20.mint(
-      C,
-      pkA,
-      S_A_C,
-      // await getVkId('MintFToken'),
-      {
-        erc20Address,
-        account: accounts[0],
-        fTokenShieldJson,
-        fTokenShieldAddress,
-      },
-      {
-        codePath: `${process.cwd()}/code/gm17/ft-mint/out`,
-        outputDirectory: `${process.cwd()}/code/gm17/ft-mint`,
-        pkPath: `${process.cwd()}/code/gm17/ft-mint/proving.key`,
-      },
-    );
-    zInd1 = parseInt(zIndex, 10);
-    console.log('salt:', S_A_C);
-    console.log('pkA:', pkA);
-    console.log('expected commitment:', Z_A_C);
-    console.log('commitment index:', zInd1);
-    expect(Z_A_C).toEqual(zTest);
-  });
-
-  test('Should transfer ERC-20 commitments of various values to ONE receipient and get change', async () => {
-    // the E's becomes Bobs'.
-    const bal1 = await controller.getBalance(accounts[0]);
-    const inputCommitment = { value: C, salt: S_A_C, commitment: Z_A_C, commitmentIndex: zInd1 };
-
-    for (let i = 0; i < E.length; i++) {
-      outputCommitments[i] = { value: E[i], salt: S_B_E[i] };
+if (process.env.HASH_TYPE === 'mimc') {
+  beforeAll(async () => {
+    if (!(await bc.isConnected())) await bc.connect();
+    accounts = await (await bc.connection()).eth.getAccounts();
+    const { contractJson, contractInstance } = await getTruffleContractInstance('FTokenShield');
+    erc20Address = await getContractAddress('FToken');
+    const erc20AddressPadded = `0x${utils.strip0x(erc20Address).padStart(64, '0')}`;
+    fTokenShieldAddress = contractInstance.address;
+    fTokenShieldJson = contractJson;
+    for (let i = 0; i < PROOF_LENGTH; i++) {
+      pkB[i] = utils.strip0x(utils.hash(skB[i]));
     }
-    const response = await erc20.simpleFungibleBatchTransfer(
-      inputCommitment,
-      outputCommitments,
-      pkB, // deliberately the same key x 20 for consolidation transfer
-      skA,
-      // await getVkId('SimpleBatchTransferFToken'),
-      {
-        erc20Address,
-        account: accounts[0],
-        fTokenShieldJson,
-        fTokenShieldAddress,
-      },
-      {
-        codePath: `${process.cwd()}/code/gm17/ft-batch-transfer/out`,
-        outputDirectory: `${process.cwd()}/code/gm17/ft-batch-transfer`,
-        pkPath: `${process.cwd()}/code/gm17/ft-batch-transfer/proving.key`,
-      },
-    );
-
-    zInd2 = parseInt(response.maxOutputCommitmentIndex, 10);
-    outputCommitments.commitment = response.outputCommitments.commitment;
-    const bal2 = await controller.getBalance(accounts[0]);
-    const wei = parseInt(bal1, 10) - parseInt(bal2, 10);
-    console.log('gas consumed was', wei / 20e9);
-    console.log('approx total cost in USD @$200/ETH was', wei * 200e-18);
-    console.log('approx per transaction cost in USD @$200/ETH was', (wei * 200e-18) / 20);
+    pkB = await Promise.all(pkB);
+    S_A_C = await utils.rndHex(32);
+    pkA = utils.strip0x(utils.hash(skA));
+    Z_A_C = utils.concatenateThenHash(erc20AddressPadded, C, pkA, S_A_C);
   });
+  // eslint-disable-next-line no-undef
+  describe('f-token-controller.js tests', () => {
+    // Alice has C + D to start total = 50 ETH
+    // Alice sends Bob E and gets F back (Bob has 40 ETH, Alice has 10 ETH)
+    // Bob then has E+G at total of 70 ETH
+    // Bob sends H to Alice and keeps I (Bob has 50 ETH and Alice has 10+20=30 ETH)
+    test('Should be correcly configurated to use MiMC', async () => {
+      expect(process.env.HASH_TYPE).toEqual('mimc');
+    });
+    test('Should create 10000 tokens in accounts[0]', async () => {
+      // fund some accounts with FToken
+      const AMOUNT = 10000;
+      const bal1 = await controller.getBalance(accounts[0]);
+      await controller.buyFToken(AMOUNT, accounts[0]);
+      const bal2 = await controller.getBalance(accounts[0]);
+      expect(AMOUNT).toEqual(bal2 - bal1);
+    });
 
-  test('Should consolidate the 20 commitments just created', async () => {
-    const pkE = await utils.rndHex(32); // public key of Eve, who we transfer to
-    const inputCommitments = [];
-    for (let i = 0; i < E.length; i++) {
-      inputCommitments[i] = {
-        value: E[i],
-        salt: S_B_E[i],
-        commitment: outputCommitments[i].commitment,
-        commitmentIndex: zInd2 - E.length + i + 1,
-      };
-    }
-    const outputCommitment = { value: C, salt: await utils.rndHex(32) };
+    test('Should mint an ERC-20 commitment Z_A_C for Alice of value C', async () => {
+      const { commitment: zTest, commitmentIndex: zIndex } = await erc20.mint(
+        C,
+        pkA,
+        S_A_C,
+        // await getVkId('MintFToken'),
+        {
+          erc20Address,
+          account: accounts[0],
+          fTokenShieldJson,
+          fTokenShieldAddress,
+        },
+        {
+          codePath: `${process.cwd()}/code/gm17/ft-mint/out`,
+          outputDirectory: `${process.cwd()}/code/gm17/ft-mint`,
+          pkPath: `${process.cwd()}/code/gm17/ft-mint/proving.key`,
+        },
+      );
+      zInd1 = parseInt(zIndex, 10);
+      console.log('salt:', S_A_C);
+      console.log('pkA:', pkA);
+      console.log('expected commitment:', Z_A_C);
+      console.log('commitment index:', zInd1);
+      expect(Z_A_C).toEqual(zTest);
+    });
 
-    const response = await erc20.consolidationTransfer(
-      inputCommitments,
-      outputCommitment,
-      pkE,
-      skB[0],
-      {
-        erc20Address,
-        account: accounts[0],
-        fTokenShieldJson,
-        fTokenShieldAddress,
-      },
-      {
-        codePath: `${process.cwd()}/code/gm17/ft-consolidation-transfer/out`,
-        outputDirectory: `${process.cwd()}/code/gm17/ft-consolidation-transfer`,
-        pkPath: `${process.cwd()}/code/gm17/ft-consolidation-transfer/proving.key`,
-      },
-    );
-    const consolidatedCommitment = response.outputCommitment;
-    console.log('Output commitment:', consolidatedCommitment.commitment);
+    test('Should transfer ERC-20 commitments of various values to ONE receipient and get change', async () => {
+      // the E's becomes Bobs'.
+      const bal1 = await controller.getBalance(accounts[0]);
+      const inputCommitment = { value: C, salt: S_A_C, commitment: Z_A_C, commitmentIndex: zInd1 };
+
+      for (let i = 0; i < E.length; i++) {
+        outputCommitments[i] = { value: E[i], salt: S_B_E[i] };
+      }
+      const response = await erc20.simpleFungibleBatchTransfer(
+        inputCommitment,
+        outputCommitments,
+        pkB, // deliberately the same key x 20 for consolidation transfer
+        skA,
+        // await getVkId('SimpleBatchTransferFToken'),
+        {
+          erc20Address,
+          account: accounts[0],
+          fTokenShieldJson,
+          fTokenShieldAddress,
+        },
+        {
+          codePath: `${process.cwd()}/code/gm17/ft-batch-transfer/out`,
+          outputDirectory: `${process.cwd()}/code/gm17/ft-batch-transfer`,
+          pkPath: `${process.cwd()}/code/gm17/ft-batch-transfer/proving.key`,
+        },
+      );
+
+      zInd2 = parseInt(response.maxOutputCommitmentIndex, 10);
+      outputCommitments.commitment = response.outputCommitments.commitment;
+      const bal2 = await controller.getBalance(accounts[0]);
+      const wei = parseInt(bal1, 10) - parseInt(bal2, 10);
+      console.log('gas consumed was', wei / 20e9);
+      console.log('approx total cost in USD @$200/ETH was', wei * 200e-18);
+      console.log('approx per transaction cost in USD @$200/ETH was', (wei * 200e-18) / 20);
+    });
+
+    test('Should consolidate the 20 commitments just created', async () => {
+      const pkE = await utils.rndHex(32); // public key of Eve, who we transfer to
+      const inputCommitments = [];
+      for (let i = 0; i < E.length; i++) {
+        inputCommitments[i] = {
+          value: E[i],
+          salt: S_B_E[i],
+          commitment: outputCommitments[i].commitment,
+          commitmentIndex: zInd2 - E.length + i + 1,
+        };
+      }
+      const outputCommitment = { value: C, salt: await utils.rndHex(32) };
+
+      const response = await erc20.consolidationTransfer(
+        inputCommitments,
+        outputCommitment,
+        pkE,
+        skB[0],
+        {
+          erc20Address,
+          account: accounts[0],
+          fTokenShieldJson,
+          fTokenShieldAddress,
+        },
+        {
+          codePath: `${process.cwd()}/code/gm17/ft-consolidation-transfer/out`,
+          outputDirectory: `${process.cwd()}/code/gm17/ft-consolidation-transfer`,
+          pkPath: `${process.cwd()}/code/gm17/ft-consolidation-transfer/proving.key`,
+        },
+      );
+      const consolidatedCommitment = response.outputCommitment;
+      console.log('Output commitment:', consolidatedCommitment.commitment);
+    });
   });
-});
+} else {
+  describe('Consolidation MIMC test disabled', () => {
+    test('HASH_TYPE env variable is set to `sha`', () => {
+      expect(process.env.HASH_TYPE).toEqual('sha');
+    });
+  });
+}
