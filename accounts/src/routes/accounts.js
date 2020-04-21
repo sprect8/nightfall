f0abb620dfd0642a0a5739bf49713d64d6cc4428
vishnu.ps <vishnu.ps@gds.ey.com>
2020-04-21 12:27:08 +0530

    fix: api documentation update for accounts microservice

--- accounts/src/routes/accounts.js
+++ accounts/src/routes/accounts.js
@@ -9,12 +9,24 @@ import {
 const router = express.Router({ mergeParams: true });
 
 /**
- * This function is to create an account
+ * @api {post} /createAccount
+ * @apiDescription This function is to create an account
+ * @apiVersion 1.0.0
+ * @apiName SignUp
+ * @apiGroup User
+ *
+ * @apiParam (Request body) (Login) {String} password User specific password
+ * @apiExample {json} Example usage:
  * req.body : {
  *    "password":"b"
  * }
  *
- * res.data : "0x48fB6f41fF365C8FE4450ED294876Dfa664F0416"
+ * @apiSuccess [address] address Address Object returned as User address.
+ * @apiSuccessExample {json} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "0x48fB6f41fF365C8FE4450ED294876Dfa664F0416"
+ *  }
  *
  * @param {*} req
  * @param {*} res
@@ -34,7 +46,25 @@ async function createAccount(req, res, next) {
 }
 
 /**
- * This function is to get accout balance
+ * @api {get} /getAccountBalance
+ * @apiDescription This function is to get account balance
+ * @apiVersion 1.0.0
+ * @apiName getAccountBalance
+ * @apiGroup User
+ *
+ * @apiParam (Request query) {Object} [address]  accountAddress   Address of the account
+ * @apiExample {json} Example usage:
+ * req.query : {
+ *  accountAddress: "0x48fB6f41fF365C8FE4450ED294876Dfa664F0416"
+ * }
+ *
+ * @apiSuccess {Number} balance Account balance.
+ * @apiSuccessExample {json} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "1000000000000"
+ *  }
+ *
  * @param {*} req
  * @param {*} res
  */
@@ -50,6 +80,35 @@ async function getAccountBalance(req, res, next) {
  * @param {*} req
  * @param {*} res
  */
+
+/**
+ * @api {get} /transferEther
+ * @apiDescription This function is to transfer ether to an account
+ * @apiVersion 1.0.0
+ * @apiName transferEther
+ * @apiGroup User
+ *
+ * @apiParam (Request query) {Object} [address] from      Sender account address
+ * @apiParam (Request query) {Object} [address] address   Receiver account address
+ * @apiParam (Request query) {Number}           amount    Amount transferring
+ *
+ * @apiExample {json} Example usage:
+ * req.query : {
+ *  from: "0x48fB6f41fF365C8FE4450ED294876Dfa664F0416",
+ *  amount: "0000000000014",
+ *  address: "0x28fB6f41fF365C8FE4450ED294876Dfa664F0753"
+ * }
+ *
+ * @apiSuccess {String} txHash Hash of the transaction.
+ * @apiSuccessExample {json} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    txHash: "0xef95f2f1ed3ca60b048b4bf67cde2195961e0bba6f70bcbea9a2c4e133e34b46"
+ *  }
+ *
+ * @param {*} req
+ * @param {*} res
+ */
 async function transferEther(req, res, next) {
   const { from, amount, address } = req.body;
   try {
@@ -62,15 +121,26 @@ async function transferEther(req, res, next) {
 }
 
 /**
- * This function is to unloack an account
+ * @api {post} /unlockUserAccount
+ * @apiDescription This function is to unloack an account
+ * @apiVersion 1.0.0
+ * @apiName unlockUserAccount
+ * @apiGroup User
+ *
+ * @apiParam (Request body) address address Address of User
+ * @apiParam (Request body) {String} password User specific password
+ * @apiExample {json} Example usage:
  * req.body : {
  *    "address":"0x48fB6f41fF365C8FE4450ED294876Dfa664F0416",
  *    "password":"b"
  * }
  *
- * res.data : {
+ * @apiSuccess {String} message Successfully unlocked message.
+ * @apiSuccessExample {json} Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
  *    "message":"Unlocked"
- * }
+ *  }
  *
  * @param {*} req
  * @param {*} res
