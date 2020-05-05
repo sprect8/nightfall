d762fc37dcd46a7fbd74904d7473ab87df5f83b9
Liju Jose <liju.jose@gds.ey.com>
2020-05-06 00:27:46 +0530

    refactor(integration-test): timeout increased

--- integration-test/test.js
+++ integration-test/test.js
@@ -668,7 +668,7 @@ describe('****** Integration Test ******\n', function() {
       if (!process.env.MIMC) {
         this.skip();
       }
-      setTimeout(done, 30000);
+      setTimeout(done, 10000);
     });
     context(`${bob.name} tasks: `, function() {
       /*
