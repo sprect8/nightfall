ea6b8b3286a2fdc9bf2179f5195f84fe785d6afe
Vishnu PS <38682769+vishnusudarshan@users.noreply.github.com>
2020-05-13 14:50:15 +0530

    feat: error logger using winston package

--- database/src/logger.js
+++ database/src/logger.js
@@ -1,6 +1,17 @@
 import { createLogger, format, transports } from 'winston';
-import mongoose from 'mongoose';
+import { inspect } from 'util';
 import config from 'config';
+
+function isPrimitive(val) {
+  return val === null || (typeof val !== 'object' && typeof val !== 'function');
+}
+
+function formatWithInspect(val) {
+  const prefix = isPrimitive(val) ? '' : '\n';
+  const shouldFormat = typeof val !== 'string';
+
+  return prefix + (shouldFormat ? inspect(val, { depth: null, colors: true }) : val);
+}
 
 const consoleLogger = {
   info: message => console.log(message),
@@ -12,11 +23,20 @@ let winstonLogger;
 if (config.get('isLoggerEnable')) {
   winstonLogger = createLogger({
     level: 'debug',
-    format: format.combine(format.colorize(), format.simple()),
+    format: format.combine(
+      format.timestamp(),
+      format.errors({ stack: true }),
+      format.colorize(),
+      format.printf(info => {
+        const msg = formatWithInspect(info.message);
+        const splatArgs = info[Symbol.for('splat')] || [];
+        const rest = splatArgs.map(data => formatWithInspect(data)).join(' ');
+
+        return `${info.level}: ${msg} ${rest}`;
+      }),
+    ),
     transports: [new transports.Console()],
   });
-
-  mongoose.set('debug', true);
 }
 
 const logger = winstonLogger || consoleLogger;
